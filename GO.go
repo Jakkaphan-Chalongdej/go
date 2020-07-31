799e3215a86ebc874dcf426a745905fa9f0dc9da
Momotari <46018667+NumberNine9s@users.noreply.github.com>
2020-08-01 00:39:43 +0700

    message

---
+++ GO.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+func main() {
+	var a, b int
+
+	fmt.Println("input A :")
+	fmt.Scanln(&a)
+	fmt.Println("input B :")
+	fmt.Scanln(&b)
+
+	if a > b {
+		c := a - b
+		fmt.Print(c)
+	}
+	else if a < b {
+		c := b - a
+		fmt.Print(c)
+	}
+}*/
+func main() {
+
+	for i := 1; i < 16; i++ {
+		fmt.Print(i)
+		fmt.Print(i)
+		fmt.Print(i)
+		fmt.Print(i)
+		fmt.Print(i)
+
+	}
+}
